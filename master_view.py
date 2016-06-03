beb6e005f6b2c3135faa0b1987250e9d194a1661
jordanlam <jordan.lam95@gmail.com>
2016-06-04 01:01:09 +1000

    Changing PKCS1_v1_5 to PKCS1_OAEP protocol
    PKCS1_OAEP is more stronger in its security

--- master_view.py
+++ master_view.py
@@ -1,6 +1,6 @@
 import os
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Hash import SHA256
 
 #function to generate and return public key
@@ -14,18 +14,14 @@ def privkey_gen():
 def encrypt_valuables(m):
     #hash the msg to fix length
     h = SHA256.new(m)
-    #imports public key encoded in standard form
+    #imports public key half encoded in standard form
     pubkey = RSA.importKey(open('pubkey.der').read())
     #using the public key to encrypt the hashed msg
-    cipher = PKCS1_v1_5.new(pubkey)
+    cipher = PKCS1_OAEP.new(pubkey)
     ciphertext = cipher.encrypt(h.digest())
 
 def decrypt_valuables(f):
-    # TODO: For Part 2, you'll need to decrypt the contents of this file
-    # The existing scheme uploads in plaintext
-    # As such, we just convert it back to ASCII and print it out
-    decoded_text = str(f, 'ascii')
-    print(decoded_text)
+    
 
 
 if __name__ == "__main__":
