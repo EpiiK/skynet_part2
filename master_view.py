4984aa19d7940666492f7085db622f1750fedd9f
jordanlam <jordan.lam95@gmail.com>
2016-06-03 22:29:43 +1000

    Added functions
    Read comments for description of the functions

--- master_view.py
+++ master_view.py
@@ -1,5 +1,16 @@
 import os
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
 
+#function to generate and return public key
+def pubkey_gen():
+    
+#function to generate and return private key
+def privkey_gen():
+    
+#encrypt the msg which takes the public key and returns ciphertext
+def encrypt_valuables(pubkey):
+    
 
 def decrypt_valuables(f):
     # TODO: For Part 2, you'll need to decrypt the contents of this file
